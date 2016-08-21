1c5bc245818810ef913c107ba51d413bf7026064
Talles Nascimento Rodrigues <Talles Nascimento Rodrigues>
2016-08-21 10:05:54 -0300

    POST_with parameters

--- app/src/main/java/com/example/tallesrodrigues/medicament11/LoginActivity.java
+++ app/src/main/java/com/example/tallesrodrigues/medicament11/LoginActivity.java
@@ -1,19 +1,25 @@
 package com.example.tallesrodrigues.medicament11;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button loginButton;
+    private Button loginButton,signupButton;
     private EditText login;
     private EditText password;
     private String pass;
     DatabaseController crud ;
+    SharedPreferences sharedPreferences;
+    public static final String MyPREFERENCES = "log_memory" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +29,26 @@ public class LoginActivity extends AppCompatActivity {
         loginButton = (Button) findViewById(R.id.button_login);
         login = (EditText)findViewById(R.id.editText);
         password = (EditText)findViewById(R.id.editText2);
+        signupButton =(Button)findViewById(R.id.signupButton);
 
-        /**/
+        sharedPreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+        String restored = sharedPreferences.getString("login",null);
+        if (restored!=null){
+          login.setText(sharedPreferences.getString("login",""));
+          password.setText(sharedPreferences.getString("password",""));
+        }
+
+        /*Start Service to load data on background*/
         Intent mServiceIntent = new Intent(this,AWS.class);
-        mServiceIntent.putExtra("Id",login.getText().toString());
+        mServiceIntent.putExtra("login",login.getText().toString());
+        mServiceIntent.putExtra("password",pass);
         this.startService(mServiceIntent);
 
-        crud = new DatabaseController(getBaseContext());
-        crud.insertData(1, "medicamento", "concentracao", 3, "dosagem_tipo", 1,1,
-        0, 3, "tipo", 4, "duracao_tipo", "status", "solteiro");
 
 
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent mainIntent = new Intent(LoginActivity.this,Acompanhamento.class);
@@ -47,6 +60,16 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        signupButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor ed = sharedPreferences.edit();
+                ed.putString("login", login.getText().toString());
+                ed.putString("password",password.getText().toString());
+                ed.apply();
+                Toast.makeText(getApplication(),"Succeed",Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 

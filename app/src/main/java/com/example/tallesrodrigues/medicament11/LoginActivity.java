ea77e0928769a8580ad013cf381a2cd3be3b892d
Talles Nascimento Rodrigues <Talles Nascimento Rodrigues>
2016-08-22 00:17:08 -0300

    Login and SignUp working

--- app/src/main/java/com/example/tallesrodrigues/medicament11/LoginActivity.java
+++ app/src/main/java/com/example/tallesrodrigues/medicament11/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.tallesrodrigues.medicament11;
 
+import android.app.ProgressDialog;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -12,14 +15,28 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
     private Button loginButton,signupButton;
     private EditText login;
     private EditText password;
-    private String pass;
+
     DatabaseController crud ;
     SharedPreferences sharedPreferences;
     public static final String MyPREFERENCES = "log_memory" ;
+    public static final String serverURL = "http://104.197.208.41:8080";
+    public static final String postOpSignup = "/sign_up";
+    public static final String postOpLogin = "/login";
+    boolean isValid = false;
+    private String email, pass, authentication = "";
+    int ope;
+
+    ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,32 +48,25 @@ public class LoginActivity extends AppCompatActivity {
         password = (EditText)findViewById(R.id.editText2);
         signupButton =(Button)findViewById(R.id.signupButton);
 
-        sharedPreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        progressDialog = new ProgressDialog(LoginActivity.this, R.style.AppTheme);
+
+        sharedPreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_MULTI_PROCESS);
         crud = new DatabaseController(getBaseContext());
 
-        String restored = sharedPreferences.getString("login",null);
+        String restored = sharedPreferences.getString("email", null);
         if (restored!=null){
-          login.setText(sharedPreferences.getString("login",""));
+            login.setText(sharedPreferences.getString("email", ""));
           password.setText(sharedPreferences.getString("password",""));
         }
-
-        /*Start Service to load data on background*/
-        Intent mServiceIntent = new Intent(this,AWS.class);
-        mServiceIntent.putExtra("email", login.getText().toString());
-        mServiceIntent.putExtra("password",pass);
-        this.startService(mServiceIntent);
-
-
 
 
         loginButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent mainIntent = new Intent(LoginActivity.this,Acompanhamento.class);
-                mainIntent.putExtra("User",login.getText().toString());
-                pass = SHA1.sha1Hash(login.getText().toString());
-                Log.e("Hash ",pass);
-                startActivity(mainIntent);
+                email = login.getText().toString();
+                pass = password.getText().toString();
+                ope = 1;
+                signup();
 
             }
         });
@@ -64,18 +74,174 @@ public class LoginActivity extends AppCompatActivity {
         signupButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences.Editor ed = sharedPreferences.edit();
-                String email = login.getText().toString();
-                String pass = password.getText().toString();
+                email = login.getText().toString();
+                pass = password.getText().toString();
+                ope = 2;
+                signup();
+                if (isValid) {
+                    startAcompanhamento();
+                    // startAWS();
+                }
 
-                ed.putString("email", email);
-                ed.putString("password", pass);
-                ed.apply();
-                crud.insertData(email, pass);
-                Toast.makeText(getApplication(),"Succeed",Toast.LENGTH_LONG).show();
             }
         });
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        // disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void startAcompanhamento() {
+        Intent mainIntent = new Intent(LoginActivity.this, Acompanhamento.class);
+        mainIntent.putExtra("login", login.getText().toString());
+        startActivity(mainIntent);
+    }
+
+    public void startAWS() {
+        /*Start Service to load data on background*/
+        Intent mServiceIntent = new Intent(this, AWS.class);
+        mServiceIntent.putExtra("email", login.getText().toString());
+        mServiceIntent.putExtra("password", pass);
+        this.startService(mServiceIntent);
+    }
+
+    public void signup() {
+
+        progressDialog.setIndeterminate(true);
+
+        progressDialog.setMessage("Processing...");
+        progressDialog.show();
+
+        if (ope == 1) {
+            authentication = "login";
+            new android.os.Handler().postDelayed(
+                    new Runnable() {
+                        public void run() {
+                            // On complete call either onSignupSuccess or onSignupFailed
+                            // depending on success
+                            new sendLogInfo().execute(serverURL + login);
+                            //onSignupSuccess();
+                            // onSignupFailed();
+                        }
+                    }, 3000);
+        } else {
+            if (ope == 2) {
+                authentication = "signup";
+                new android.os.Handler().postDelayed(
+                        new Runnable() {
+                            public void run() {
+                                // On complete call either onSignupSuccess or onSignupFailed
+                                // depending on success
+                                new sendLogInfo().execute(serverURL + postOpSignup);
+                                //onSignupSuccess();
+                                // onSignupFailed();
+                                progressDialog.dismiss();
+                            }
+                        }, 3000);
+            }
+        }
+
+    }
+
+    public void onSignupSuccess() {
+        setResult(RESULT_OK, null);
+
+    }
+
+    private class sendLogInfo extends AsyncTask<String, Void, Void> {
+        private String Content;
+        private final HttpClient Client = new DefaultHttpClient();
+        private String Error;
+        ContentValues values;
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            values = new ContentValues();
+            try {
+                values.put("email", email);
+                values.put("password", pass);
+                values.put("qtd", 0);
+            } catch (NullPointerException n) {
+                Log.e("Still null ", n.toString());
+            }
+
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            // Creating service handler class instance
+            ServiceHandler sh = new ServiceHandler();
+
+            // Making a request to url and getting response
+            try {
+                Log.i("Login sent ", values.getAsString("email"));
+            } catch (NullPointerException n) {
+                n.printStackTrace();
+            }
+            String jsonStr = "";
+
+            if (ope == 1) {
+                jsonStr = sh.makeServiceCall(serverURL + postOpLogin, ServiceHandler.POST, values);
+            } else {
+                if (ope == 2) {
+                    jsonStr = sh.makeServiceCall(serverURL + postOpSignup, ServiceHandler.POST, values);
+                }
+            }
+
+            Log.i("Response: ", "> " + jsonStr);
+
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonStr);
+
+                    if (jsonObject.getBoolean("status")) {
+                        Log.i("Status", jsonObject.getString("msg"));
+                        isValid = true;
+                    } else {
+                        isValid = false;
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            //Log.e("WebService Returned", "Output: "+ Content);
+            try {
+                SharedPreferences.Editor ed = sharedPreferences.edit();
+                Log.i("authentication", authentication);
+                if (isValid) {
+                    ed.putString("email", email);
+                    ed.putString("password", pass);
+                    ed.apply();
+                    crud.insertData(email, pass);
+
+                    startAcompanhamento();
+                    progressDialog.dismiss();
+                    // startAWS();
+
+                }
+                progressDialog.dismiss();
+                if (!isValid) {
+                    Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+                }
+            } catch (NullPointerException n) {
+                n.printStackTrace();
+            }
+
+        }
     }
 
     @Override
